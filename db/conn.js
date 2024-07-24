d05932753a258542c9ebf4073834b7ad0f0c73f5
devdhananjaykgp <167438373+devdhananjaykgp@users.noreply.github.com>
2024-07-24 22:50:02 +0530

    adding mongoDB database for register

---
+++ db/conn.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/consoletechuser"
+    ,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    // useCreateIndex:true
+}
+).then(()=>{
+    console.log("Connection Sucessfull")
+}).catch((e)=>{
+    console.log("No connection")
+})
--- src/app.js
+++ src/app.js
@@ -2,12 +2,16 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const path = require("path");
+require("../db/conn");
+const Register = require("../models/register");
 
 const staticPath = path.join(__dirname,"../public");
 const templatePath = path.join(__dirname,"../templates/views");
 
 
+
 app.use(express.static(staticPath));
+app.use(express.urlencoded({extended:false}));
 
 app.set("view engine","hbs");
 app.set("views",templatePath);
@@ -23,6 +27,34 @@ app.get("/login",(req,res)=>{
 app.get("/register",(req,res)=>{
     res.render("register");
 });
+
+app.post("/register", async(req, res)=>{
+    try{
+
+        const password = req.body.password;
+        const cpassword = req.body.confirmpassword;
+
+        if(password === cpassword){
+            const registerUser = new Register({
+                name : req.body.name,
+                number : req.body.number,
+                email : req.body.email,
+                password : password,
+                confirmpassword : cpassword,
+            })
+
+            const register = await registerUser.save();
+            res.status(200).render("index");
+        }else{
+            res.send("Password are not match")
+        }
+
+
+
+    }catch(error) {
+        res.status(400).send(error)
+    }
+})
 
 app.get("*",(req,res)=>{
     res.render("404");
