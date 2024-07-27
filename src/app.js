0f0d280fc07f7124b7094432c1b1901b950371d1
devdhananjaykgp <167438373+devdhananjaykgp@users.noreply.github.com>
2024-07-27 10:51:51 +0530

    login code updated

--- src/app.js
+++ src/app.js
@@ -56,6 +56,26 @@ app.post("/register", async(req, res)=>{
     }
 })
 
+app.post("/login",async(req,res)=>{
+    try {
+        const email = req.body.email;
+        const password = req.body.password;
+        const useremail = await Register.findOne({email:email});
+        
+        if(useremail.password == password){
+            res.status(201).render("index");
+        }else{
+            res.send("Invalid login details");
+        }
+        
+    } catch (error) {
+        res.status(400).send("Invalid login details")
+        
+    }
+});
+
+
+
 app.get("*",(req,res)=>{
     res.render("404");
 });
